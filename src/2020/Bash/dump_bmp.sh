7227e764b7ab79ceb01f37232b6bc934afe035b2
ragingo <save.the.world.crush@gmail.com>
2020-05-31 19:26:32 +0900

    [2020/Bash] コード整理

--- src/2020/Bash/dump_bmp.sh
+++ src/2020/Bash/dump_bmp.sh
@@ -15,36 +15,37 @@ pwd
 readonly SRC_FILE_PATH=./catman.bmp
 readonly SRC_DATA=$(bin_to_dec_str "$SRC_FILE_PATH" | trim_spaces)
 
+calc_offsets() {
+    local -n _sizes=$1
+    local -n _offsets=$2
+    local item_count=${#_sizes[@]}
+
+    array_fill offsets "$item_count" 0
+
+    for ((i=0; i < "$item_count"; i++)) do
+        if [ $i -gt 0 ]; then
+            local prev_offset=${_offsets[$((i - 1))]}
+            local prev_size=${_sizes[$((i - 1))]}
+            local offset=$((prev_offset + prev_size))
+            offsets[$i]=$offset
+        else
+            offsets[$i]=0
+        fi
+    done
+}
+
 dump_bitmap_file_header() {
     local data=$1
     local sizes=(2 4 2 2 4)
     local offsets=()
     local item_count=${#sizes[@]}
 
-    array_fill offsets "$item_count" 0
-
-    for ((i=0; i < "$item_count"; i++)) do
-        if [ $i -gt 0 ]; then
-            local prev_offset=${offsets[$((i - 1))]}
-            local prev_size=${sizes[$((i - 1))]}
-            local offset=$((prev_offset + prev_size))
-            # echo "prev offset: $prev_offset, prev size: $prev_size, offset: $offset"
-            offsets[$i]=$offset
-        else
-            offsets[$i]=0
-        fi
-    done
-
-    # echo "${offsets[@]}"
+    calc_offsets sizes offsets
 
     local ret=()
     for ((i=0; i < "$item_count"; i++)) do
-        local start=${offsets[$i]}
-        local len=${sizes[$i]}
-        local val=""
-        val=$(substr "$data" "$start" "$len")
-
-        # echo "start: $start, len: $len, val: $val"
+        local val
+        val=$(substr "$data" "${offsets[$i]}" "${sizes[$i]}")
 
         ret+=("$val")
         # ↓ これでも配列に追加できる(標準出力を追加したい場合はこれじゃないとだめそう)
@@ -55,19 +56,13 @@ dump_bitmap_file_header() {
                 echo "$val" | awk '{ printf("type: %c%c\n", $1, $2) }'
                 ;;
             1 )
-                local x=(${val// / })
-                local y=0
-                y=$(u8x4_to_u32 "${x[@]}")
-                printf "size: %'d B\n" "$y"
+                printf "size: %'d B\n" "$(u8x4_string_to_u32 "$val")"
                 ;;
             2 | 3)
                 echo "reserved"
                 ;;
             4 )
-                local x=(${val// / })
-                local y=0
-                y=$(u8x4_to_u32 "${x[@]}")
-                printf "offbits: %'d B\n" "$y"
+                printf "offbits: %'d B\n" "$(u8x4_string_to_u32 "$val")"
                 ;;
         esac
     done
--- src/2020/Bash/string.sh
+++ src/2020/Bash/string.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+split() {
+    local -n arr=$1
+    local str=$2
+    arr=(${str// / })
+}
+
+export -f split
+
+i_to_c() {
+    local i=$1
+    echo "$i" | awk '{ print("%c", $1) }'
+}
+
+substr() {
+    local str=$1
+    local start=$2
+    local len=$3
+    local end=0
+
+    end=$((start + len))
+    start=$((start + 1))
+    echo "$str" | cut -d' ' -f "$start-$end"
+}
+
+export -f substr
+
+trim_spaces() {
+    local stdin=""
+    read -r stdin
+    echo "$stdin"
+}
+
+export -f trim_spaces
+
 bin_to_dec_str() {
     local file=$1
 
@@ -23,23 +57,13 @@ u8x4_to_u32() {
 
 export -f u8x4_to_u32
 
-substr() {
+u8x4_string_to_u32() {
     local str=$1
-    local start=$2
-    local len=$3
-    local end=0
-
-    end=$((start + len))
-    start=$((start + 1))
-    echo "$str" | cut -d' ' -f "$start-$end"
+    local x=()
+    split x "$str"
+    local y=0
+    y=$(u8x4_to_u32 "${x[@]}")
+    echo "$y"
 }
 
-export -f substr
-
-trim_spaces() {
-    local stdin=""
-    read -r stdin
-    echo "$stdin"
-}
-
-export -f trim_spaces
+export -f u8x4_string_to_u32
