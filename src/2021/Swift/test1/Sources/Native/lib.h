c52af133c95b717218a4af739e658f82093c8525
ragingo <save.the.world.crush@gmail.com>
2021-03-21 19:51:47 +0900

    [2021/Swift/test1] ビジュアルスタイル有効化
    https://docs.microsoft.com/en-us/windows/win32/controls/cookbook-overview

--- src/2021/Swift/test1/Sources/Native/lib.h
+++ src/2021/Swift/test1/Sources/Native/lib.h
@@ -6,6 +6,8 @@
 #include <Windows.h>
 #include <CommCtrl.h>
 
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 #ifdef __cplusplus
 extern "C" {
 #endif
