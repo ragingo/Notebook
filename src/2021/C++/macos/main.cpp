da83cc44d360a740fdcef1cc1734418ef8555fdd
ragingo <save.the.world.crush@gmail.com>
2021-12-10 01:21:37 +0900

    [2021/C++] macos で別のアプリの標準出力を拾う実験 (pipe)

--- src/2021/C++/macos/main.cpp
+++ src/2021/C++/macos/main.cpp
@@ -4,53 +4,88 @@
 #include <string_view>
 #include <thread>
 #include <chrono>
-#include <deque>
-#include <mutex>
 #include <memory>
-#include <functional>
 #include <iostream>
+#include <unistd.h>
+#include <pthread.h>
+#include <vector>
 
-static std::mutex outputs_mutex;
+struct StartupInfo {
+    int32_t processID;
+    uint64_t threadID;
+    int32_t readPipe;
+    int32_t writePipe;
+};
 
-static std::thread createProcess(std::string name, std::deque<std::string>& outputs) {
-    // memo: https://www.ipa.go.jp/security/awareness/vendor/programmingv2/contents/c909.html
-    std::shared_ptr<FILE> pipe(
-        popen(name.c_str(), "r"),
-        [](FILE* fp) {
-            pclose(fp);
-        }
-    );
-
-    std::thread thread([pipe, &outputs]{
-        char buf[1024] = {};
-        while (!feof(pipe.get())) {
-            if (fgets(buf, sizeof(buf), pipe.get()) != nullptr) {
-                std::lock_guard<std::mutex> lock(outputs_mutex);
-                outputs.push_back(buf);
-            }
-        }
-    });
-
-    return thread;
+static int execApp(std::string name, std::vector<std::string> args) {
+    std::vector<const char*> argv;
+    argv.push_back(name.c_str());
+    for (auto arg : args) {
+        argv.push_back(arg.c_str());
+    }
+    argv.push_back(nullptr);
+    argv.shrink_to_fit();
+    return execvp(name.c_str(), (char**)argv.data());
 }
 
-static void printOutputs(std::deque<std::string>& outputs) {
-    if (outputs.empty()) {
-        return;
+static uint64_t getCurrentThreadID() {
+    uint64_t threadID;
+    pthread_threadid_np(nullptr, &threadID);
+    return threadID;
+}
+
+static bool createProcess(std::string name, std::vector<std::string> args, StartupInfo& si) {
+    int fd[2] = { -1 };
+    if (pipe(fd) < 0) {
+        return false;
     }
-    std::lock_guard<std::mutex> lock(outputs_mutex);
-    auto output = outputs.front();
-    outputs.pop_front();
-    std::cout << output << std::endl;
+
+    pid_t processID = fork();
+    printf("pid: %d\n", processID);
+
+    if (processID < 0) {
+        return false;
+    }
+    if (processID == 0) {
+        // memo: 子プロセスで実行されている
+        ::close(fd[0]);
+
+        ::dup2(fd[1], STDOUT_FILENO);
+        ::close(fd[1]);
+
+        execApp(name, args);
+        _exit(EXIT_SUCCESS);
+    }
+
+    ::close(fd[1]);
+
+    si.processID = processID;
+    si.threadID = getCurrentThreadID();
+    si.readPipe = fd[0];
+    // si.writePipe = ;
+    return true;
 }
 
 int main() {
-    const auto appName = "ping 127.0.0.1";
-    std::deque<std::string> outputs;
-    auto thread = createProcess(appName, outputs);
+    const auto appName = "ping";
+    const std::vector<std::string> args = { "127.0.0.1" };
+    StartupInfo si = {};
+
+    if (!createProcess(appName, args, si)) {
+        std::cout << "fork failed." << std::endl;
+        return EXIT_FAILURE;
+    }
 
     while(true) {
-        printOutputs(outputs);
+        char buf[256] = {};
+        int len = read(si.readPipe, buf, sizeof(buf));
+        if (len < 0) {
+            printf("read error.\n");
+        } else if (len == 0) {
+            printf("eof.\n");
+        } else {
+            printf("%s\n", buf);
+        }
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
