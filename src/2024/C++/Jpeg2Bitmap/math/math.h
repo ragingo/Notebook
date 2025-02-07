e9133d7f1b13bd2a10798df23618d2fe4cdad824
ragingo <4784032+ragingo@users.noreply.github.com>
2025-02-07 09:53:59 +0900

    [2024/C++/Jpeg2Bitmap] idct内の cosv, cv の計算回数を削減

--- src/2024/C++/Jpeg2Bitmap/math/math.h
+++ src/2024/C++/Jpeg2Bitmap/math/math.h
@@ -17,19 +17,21 @@ namespace math
         const double cv0 = 1.0 / std::sqrt(N);
         const double cu1 = std::sqrt(2.0 / M);
         const double cv1 = std::sqrt(2.0 / N);
-        double pi = std::numbers::pi;
+        constexpr double pi = std::numbers::pi;
 
         for (int y = 0; y < N; ++y) {
             for (int x = 0; x < M; ++x) {
                 double sum = 0.0;
                 for (int v = 0; v < N; ++v) {
+                    double cosv = std::cos((2 * y + 1) * v * pi / (2 * N));
+                    double cv = ((v == 0) ? cv0 : cv1) * cosv;
+
                     for (int u = 0; u < M; ++u) {
                         double cosu = std::cos((2 * x + 1) * u * pi / (2 * M));
-                        double cosv = std::cos((2 * y + 1) * v * pi / (2 * N));
-                        double cu = (u == 0) ? cu0 : cu1;
-                        double cv = (v == 0) ? cv0 : cv1;
+                        double cu = ((u == 0) ? cu0 : cu1) * cosu;
+
                         int offset = v * N + u;
-                        sum += static_cast<double>(input[offset]) * cosu * cosv * cu * cv;
+                        sum += static_cast<double>(input[offset]) * cu * cv;
                     }
                 }
                 int offset = y * N + x;
