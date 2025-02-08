1e3a6242c9f668b3ed7c9d1c8e446b322272c0f3
ragingo <4784032+ragingo@users.noreply.github.com>
2025-02-08 15:20:06 +0900

    [2024/C++/Jpeg2Bitmap] constexpr 対応の数学関数をいくつか追加

--- src/2024/C++/Jpeg2Bitmap/math/math.h
+++ src/2024/C++/Jpeg2Bitmap/math/math.h
@@ -1,12 +1,52 @@
 #pragma once
 #include <array>
 #include <concepts>
+#include <limits>
 #include <numbers>
 
 namespace math
 {
     inline constexpr double pi = std::numbers::pi;
+    inline constexpr double sqrt2 = std::numbers::sqrt2;
     inline constexpr double sqrt8 = 2.0 * std::numbers::sqrt2;
+
+    inline constexpr double abs(double x)
+    {
+        return x < 0.0 ? -x : x;
+    }
+    static_assert(abs(-1.0) == 1.0);
+    static_assert(abs(1.0) == 1.0);
+
+    inline constexpr double deg2rad(double deg)
+    {
+        return deg * pi / 180.0;
+    }
+    static_assert(abs(deg2rad(30) - (pi / 6)) <= std::numeric_limits<double>::epsilon());
+    static_assert(abs(deg2rad(45) - (pi / 4)) <= std::numeric_limits<double>::epsilon());
+    static_assert(abs(deg2rad(180) - pi) <= std::numeric_limits<double>::epsilon());
+
+    template<double x, int max = 10>
+    inline constexpr double sin()
+    {
+        double y = std::clamp(x, -2.0 * pi, 2.0 * pi);
+        double sum = y;
+        double t = y;
+        for (int n = 1; n <= max; n++) {
+            t *= -(y * y) / ((2 * n + 1) * (2 * n));
+            sum += t;
+        }
+        return sum;
+    }
+    constexpr double __sin45 = sin<deg2rad(45)>();
+    static_assert(abs(__sin45 - (1.0 / sqrt2)) <= std::numeric_limits<double>::epsilon());
+
+    template<double x, int max = 10>
+    inline constexpr double cos()
+    {
+        return sin<pi / 2.0 - x, max>();
+    }
+    constexpr double __cos45 = cos<deg2rad(45)>();
+    static_assert(abs(__cos45 - (1.0 / sqrt2)) <= std::numeric_limits<double>::epsilon());
 
     template<
         std::integral ElementType = int,
