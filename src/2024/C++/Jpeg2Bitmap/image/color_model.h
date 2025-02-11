a0423af55d1106e62497ce68698a86aa7a3b396e
ragingo <4784032+ragingo@users.noreply.github.com>
2025-02-11 22:28:43 +0900

    [2024/C++/Jpeg2Bitmap] YCbCr -> RGB 変換を整数だけで計算

--- src/2024/C++/Jpeg2Bitmap/image/color_model.h
+++ src/2024/C++/Jpeg2Bitmap/image/color_model.h
@@ -4,15 +4,20 @@
 #include <cstdint>
 #include <tuple>
 
-inline constexpr std::tuple<uint8_t, uint8_t, uint8_t> convertYCbCrToRGB(int y, int cb, int cr)
+constexpr std::tuple<uint8_t, uint8_t, uint8_t> convertYCbCrToRGB(int y, int cb, int cr)
 {
+    constexpr int coeff_r = static_cast<int>(1.402 * 65536);
+    constexpr int coeff_g_1 = static_cast<int>(0.344136 * 65536);
+    constexpr int coeff_g_2 = static_cast<int>(0.714136 * 65536);
+    constexpr int coeff_b = static_cast<int>(1.772 * 65536);
+
     cb -= 128;
     cr -= 128;
-    double r = y + 1.402 * cr;
-    double g = y - 0.344136 * cb - 0.714136 * cr;
-    double b = y + 1.772 * cb;
-    r = std::clamp(r, 0.0, 255.0);
-    g = std::clamp(g, 0.0, 255.0);
-    b = std::clamp(b, 0.0, 255.0);
+    int r = y + ((coeff_r * cr) >> 16);
+    int g = y - ((coeff_g_1 * cb + coeff_g_2 * cr) >> 16);
+    int b = y + ((coeff_b * cb) >> 16);
+    r = std::clamp(r, 0, 255);
+    g = std::clamp(g, 0, 255);
+    b = std::clamp(b, 0, 255);
     return { static_cast<uint8_t>(r), static_cast<uint8_t>(g), static_cast<uint8_t>(b) };
 }
