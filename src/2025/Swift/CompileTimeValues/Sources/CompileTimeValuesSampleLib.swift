e0eeb6602cea2d8d4a60ae7977783de7ef21e276
ragingo <4784032+ragingo@users.noreply.github.com>
2025-06-22 00:31:01 +0900

    [2025/Swift/CompileTimeValuesSampleLib] ライブラリ関数を公開、テストコードを整理

--- src/2025/Swift/CompileTimeValues/Sources/CompileTimeValuesSampleLib.swift
+++ src/2025/Swift/CompileTimeValues/Sources/CompileTimeValuesSampleLib.swift
@@ -4,7 +4,7 @@ import Foundation
 
 private func runtimeFunction() {}
 
-@const func static_assert(@const _ condition: Bool) -> Bool {
+@const @discardableResult public func static_assert(@const _ condition: Bool) -> Bool {
     if !condition {
         runtimeFunction()
         return false
@@ -12,75 +12,94 @@ private func runtimeFunction() {}
     return true
 }
 
-enum Math {
-    @const static let pi = 3.141592653589793238462643383279502884197169399375105820974944592307816
-    @const static let epsilon = Double.ulpOfOne
+public enum Math {
+    @const public static let pi = 3.141592653589793238462643383279502884197169399375105820974944592307816
+    @const public static let epsilon = Double.ulpOfOne
 
-    @const static func add<T: Numeric>(@const _ lhs: T, @const _ rhs: T) -> T {
+    @const public static func add<T: Numeric>(@const _ lhs: T, @const _ rhs: T) -> T {
         return lhs + rhs
     }
 
-    @const static func sub<T: Numeric>(@const _ lhs: T, @const _ rhs: T) -> T {
+    @const public static func sub<T: Numeric>(@const _ lhs: T, @const _ rhs: T) -> T {
         return lhs - rhs
     }
 
-    @const static func mul<T: Numeric>(@const _ lhs: T, @const _ rhs: T) -> T {
+    @const public static func mul<T: Numeric>(@const _ lhs: T, @const _ rhs: T) -> T {
         return lhs * rhs
     }
 
-    @const static func div<T>(@const _ lhs: T, @const _ rhs: T) -> T where T: BinaryFloatingPoint {
+    @const public static func div<T>(@const _ lhs: T, @const _ rhs: T) -> T where T: BinaryFloatingPoint {
         return lhs / rhs
     }
 
-    @const static func div<T>(@const _ lhs: T, @const _ rhs: T) -> T where T: BinaryInteger {
+    @const public static func div<T>(@const _ lhs: T, @const _ rhs: T) -> T where T: BinaryInteger {
         return lhs / rhs
     }
 
-    @const static func abs<T>(@const _ value: T) -> T where T: Numeric, T: Comparable {
+    @const public static func abs<T>(@const _ value: T) -> T where T: Numeric, T: Comparable {
         return value < .zero ? -1 * value : value
     }
 
-    @const static func min<T: Comparable>(@const _ lhs: T, @const _ rhs: T) -> T {
+    @const public static func min<T: Comparable>(@const _ lhs: T, @const _ rhs: T) -> T {
         return lhs < rhs ? lhs : rhs
     }
 
-    @const static func max<T: Comparable>(@const _ lhs: T, @const _ rhs: T) -> T {
+    @const public static func max<T: Comparable>(@const _ lhs: T, @const _ rhs: T) -> T {
         return lhs < rhs ? rhs : lhs
     }
 
-    @const static func clamp<T: Comparable>(@const value: T, @const from: T, @const to: T) -> T {
+    @const public static func clamp<T: Comparable>(@const value: T, @const from: T, @const to: T) -> T {
         @const let clamped1 = value < from ? from : value
         @const let clamped2 = clamped1 > to ? to : clamped1
         return clamped2
     }
 
-    @const static func toRad(@const deg: Double) -> Double {
+    @const public static func toRad(@const deg: Double) -> Double {
         return deg * pi / 180.0
+    }
+
+    @const public static func toDeg(@const rad: Double) -> Double {
+        return rad * 180.0 / pi
     }
 
 } // enum Math
 
-@const let a: Int = Math.add(2, 4)
-@const let b: Int = Math.sub(a, 1)
-@const let c: Int = Math.mul(b, 2)
-@const let d: Float = Math.div(Float(c), 2.0)
-@const let e: Int = Int(d)
-@const let f: Bool = static_assert(e == 5)
 
-@const let g = Math.abs(-5)
-@const let g_test = static_assert(g == 5)
-@const let h = Math.min(10, 1)
-@const let h_test = static_assert(h == 1)
-@const let i = Math.max(10, 1)
-@const let i_test = static_assert(i == 10)
-@const let j = Math.clamp(value: 10, from: 1, to: 5)
-@const let j_test = static_assert(j == 5)
-@const let k = Math.pi
-@const let l = Int(k * 100)
-@const let l_test = static_assert(l == 314)
+#if true
 
-@const let m = Math.toRad(deg: 30.0)
-@const let n = Math.pi / 6.0
-@const let o = m - n
-@const let p = Math.abs(o)
-@const let p_test = static_assert(p <= Math.epsilon)
+// @const func testAdd() {
+//     @const let a = Math.add(2, 3)
+//     @const let r = static_assert(a == 6) // コンパイルエラーにならない
+// }
+
+@const let add_result = Math.add(2, 3)
+@const let add_assertion = static_assert(add_result == 5)
+
+@const let sub_result = Math.sub(5, 3)
+@const let sub_assertion = static_assert(sub_result == 2)
+
+@const let mul_result = Math.mul(2, 3)
+@const let mul_assertion = static_assert(mul_result == 6)
+
+@const let div_result = Math.div(6, 3)
+@const let div_assertion = static_assert(div_result == 2)
+
+@const let abs_result = Math.abs(-5)
+@const let abs_assertion = static_assert(abs_result == 5)
+
+@const let min_result = Math.min(2, 3)
+@const let min_assertion = static_assert(min_result == 2)
+
+@const let max_result = Math.max(2, 3)
+@const let max_assertion = static_assert(max_result == 3)
+
+@const let clamp_result = Math.clamp(value: 5, from: 2, to: 4)
+@const let clamp_assertion = static_assert(clamp_result == 4)
+
+@const let toRad_result = Math.toRad(deg: 180)
+@const let toRad_assertion = static_assert(toRad_result == Math.pi)
+
+@const let toDeg_result = Math.toDeg(rad: Math.pi)
+@const let toDeg_assertion = static_assert(toDeg_result == 180.0)
+
+#endif
--- src/2025/Swift/CompileTimeValues/Tests/CompileTimeValuesSampleLibTests.swift
+++ src/2025/Swift/CompileTimeValues/Tests/CompileTimeValuesSampleLibTests.swift
@@ -1,2 +1,31 @@
-import Testing
-@testable import CompileTimeValuesSampleLib
+import CompileTimeValuesSampleLib
+
+@const let add_result = Math.add(2, 3)
+@const let add_assertion = static_assert(add_result == 5)
+
+@const let sub_result = Math.sub(5, 3)
+@const let sub_assertion = static_assert(sub_result == 2)
+
+@const let mul_result = Math.mul(2, 3)
+@const let mul_assertion = static_assert(mul_result == 6)
+
+@const let div_result = Math.div(6, 3)
+@const let div_assertion = static_assert(div_result == 2)
+
+@const let abs_result = Math.abs(-5)
+@const let abs_assertion = static_assert(abs_result == 5)
+
+@const let min_result = Math.min(2, 3)
+@const let min_assertion = static_assert(min_result == 2)
+
+@const let max_result = Math.max(2, 3)
+@const let max_assertion = static_assert(max_result == 3)
+
+@const let clamp_result = Math.clamp(value: 5, from: 2, to: 4)
+@const let clamp_assertion = static_assert(clamp_result == 4)
+
+@const let toRad_result = Math.toRad(deg: 180)
+@const let toRad_assertion = static_assert(toRad_result == Math.pi)
+
+@const let toDeg_result = Math.toDeg(rad: Math.pi)
+@const let toDeg_assertion = static_assert(toDeg_result == 180.0)
