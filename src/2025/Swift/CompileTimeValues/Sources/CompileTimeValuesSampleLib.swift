cae6fbb545ab39f27a66b1c50165153332a14605
ragingo <4784032+ragingo@users.noreply.github.com>
2025-06-21 15:40:53 +0900

    [2025/Swift/CompileTimeValuesSampleLib] コンパイル時計算の関数を Math に移動

--- src/2025/Swift/CompileTimeValues/Sources/CompileTimeValuesSampleLib.swift
+++ src/2025/Swift/CompileTimeValues/Sources/CompileTimeValuesSampleLib.swift
@@ -1,99 +1,86 @@
 // https://gist.github.com/ragingo/d58e9ab682d72f4e668f4f019f4e91b9
 
-// このコードは、 swift build ではコンパイルできない！
-// swiftc を直接使ってビルドすること。
-
 import Foundation
 
-func runtimeFunction() {}
+private func runtimeFunction() {}
 
 @const func static_assert(@const _ condition: Bool) -> Bool {
     if !condition {
         runtimeFunction()
-        //#error("") // compile error
         return false
     }
     return true
 }
 
-//enum Math { // クラッシュするからトップレベル関数として使うことにする
-@const func add<T: BinaryInteger>(@const _ lhs: T, @const _ rhs: T) -> T {
-  return lhs + rhs
-}
+enum Math {
+    @const static let pi = 3.141592653589793238462643383279502884197169399375105820974944592307816
+    @const static let epsilon = Double.ulpOfOne
 
-@const func sub<T: BinaryInteger>(@const _ lhs: T, @const _ rhs: T) -> T {
-  return lhs - rhs
-}
+    @const static func add<T: Numeric>(@const _ lhs: T, @const _ rhs: T) -> T {
+        return lhs + rhs
+    }
 
-@const func mul<T: BinaryInteger>(@const _ lhs: T, @const _ rhs: T) -> T {
-  return lhs * rhs
-}
+    @const static func sub<T: Numeric>(@const _ lhs: T, @const _ rhs: T) -> T {
+        return lhs - rhs
+    }
 
-@const func div<T: FloatingPoint>(@const _ lhs: T, @const _ rhs: T) -> T {
-  return lhs / rhs
-}
+    @const static func mul<T: Numeric>(@const _ lhs: T, @const _ rhs: T) -> T {
+        return lhs * rhs
+    }
 
-@const func abs<T>(@const _ value: T) -> T where T: BinaryInteger {
-    return value < T.zero ? -1 * value : value
-}
+    @const static func div<T>(@const _ lhs: T, @const _ rhs: T) -> T where T: BinaryFloatingPoint {
+        return lhs / rhs
+    }
 
-@const func abs<T>(@const _ value: T) -> T where T: FloatingPoint {
-    return value < T.zero ? -1 * value : value
-}
+    @const static func div<T>(@const _ lhs: T, @const _ rhs: T) -> T where T: BinaryInteger {
+        return lhs / rhs
+    }
 
-@const func min<T: BinaryInteger>(@const _ lhs: T, @const _ rhs: T) -> T {
-    return lhs < rhs ? lhs : rhs
-}
+    @const static func abs<T>(@const _ value: T) -> T where T: Numeric, T: Comparable {
+        return value < .zero ? -1 * value : value
+    }
 
-@const func max<T: BinaryInteger>(@const _ lhs: T, @const _ rhs: T) -> T {
-    return lhs < rhs ? rhs : lhs
-}
+    @const static func min<T: Comparable>(@const _ lhs: T, @const _ rhs: T) -> T {
+        return lhs < rhs ? lhs : rhs
+    }
 
-@const func clamp<T: BinaryInteger>(@const value: T, @const from: T, @const to: T) -> T {
-    //@const let a: T = max(value, from) // compile error
-    @const let clamped1 = value < from ? from : value
-    @const let clamped2 = clamped1 < to ? clamped1 : to
-    return clamped2
-}
+    @const static func max<T: Comparable>(@const _ lhs: T, @const _ rhs: T) -> T {
+        return lhs < rhs ? rhs : lhs
+    }
 
-@const let pi = 3.141592653589793238462643383279502884197169399375105820974944592307816
-@const let epsilon = Double.ulpOfOne
+    @const static func clamp<T: Comparable>(@const value: T, @const from: T, @const to: T) -> T {
+        @const let clamped1 = value < from ? from : value
+        @const let clamped2 = clamped1 > to ? to : clamped1
+        return clamped2
+    }
 
-@const func toRad(@const deg: Double) -> Double {
-    return deg * pi / 180.0
-}
+    @const static func toRad(@const deg: Double) -> Double {
+        return deg * pi / 180.0
+    }
 
-//} // enum Math
+} // enum Math
 
-@const let a: Int = add(2, 4)
-@const let b: Int = sub(a, 1)
-@const let c: Int = mul(b, 2)
-@const let d: Float = div(Float(c), 2.0)
+@const let a: Int = Math.add(2, 4)
+@const let b: Int = Math.sub(a, 1)
+@const let c: Int = Math.mul(b, 2)
+@const let d: Float = Math.div(Float(c), 2.0)
 @const let e: Int = Int(d)
 @const let f: Bool = static_assert(e == 5)
-//@const let f_test: Bool = static_assert(e == 6) // compile error
 
-@const let g = abs(-5)
+@const let g = Math.abs(-5)
 @const let g_test = static_assert(g == 5)
-@const let h = min(10, 1)
+@const let h = Math.min(10, 1)
 @const let h_test = static_assert(h == 1)
-@const let i = max(10, 1)
+@const let i = Math.max(10, 1)
 @const let i_test = static_assert(i == 10)
-@const let j = clamp(value: 10, from: 1, to: 5)
+@const let j = Math.clamp(value: 10, from: 1, to: 5)
 @const let j_test = static_assert(j == 5)
-//@const let k = pi() * 100 // compile error
-@const let k = pi
+@const let k = Math.pi
 @const let l = Int(k * 100)
 @const let l_test = static_assert(l == 314)
 
-@const let m = toRad(deg: 30.0)
-@const let n = pi / 6.0
+@const let m = Math.toRad(deg: 30.0)
+@const let n = Math.pi / 6.0
 @const let o = m - n
-@const let p = abs(o)
-@const let p_test = static_assert(p <= epsilon)
-
-// 関数は void 以外を返さないとだめらしい。省略もできない。
-//@const let z: Void = static_assert(y == 120) // compile error
-//@const let _ = static_assert(y == 120) // compile error
-//@const _ = static_assert(y == 120) // compile error
-//static_assert(y == 120) // compile error
+@const let p = Math.abs(o)
+@const let p_test = static_assert(p <= Math.epsilon)
